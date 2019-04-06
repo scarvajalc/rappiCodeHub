c137c7eb07a7cceb377078c8f6bbd8373d4ba603
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-05 22:20:19 -0500

    Implemented clientLogin method in repository

--- src/app/repository/index.js
+++ src/app/repository/index.js
@@ -1,5 +1,6 @@
 const Client = require('../models/client');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const registerClient = (clientData, res) => {
     Client.findOne({
@@ -26,4 +27,27 @@ const registerClient = (clientData, res) => {
     });
 };
 
-module.exports = registerClient;
+const clientLogin = (clientData, res) => {
+    Client.findOne({
+        where: {
+            email: clientData.email
+        }
+    })
+    .then(client => {
+        if(client){
+            if(bcrypt.compareSync(clientData.password, client.password)){
+                let token = jwt.sign(client.dataValues, process.env.SECRET_KEY, {
+                    expiresIn: 1500
+                });
+                res.send(token);
+            }
+        } else {
+            res.status(400).json({error: `Client does not exist`})
+        };
+    })
+    .catch(err => {
+        res.status(400).json({ error: err})
+    });  
+};
+
+module.exports = { registerClient, clientLogin};
