4575f58cca20234aad86a3806c39cc5c8f7d3ef1
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-05 22:17:57 -0500

    Removed HTTP request client data handle and findOne method in the /login endpoint - Imported login from handler and login method from repository

--- src/app/controllers/client.js
+++ src/app/controllers/client.js
@@ -1,35 +1,17 @@
 const express = require('express');
 const clients = express.Router();
 
-const handleHTTPRegister = require('../controllers/handlers/client');
-const registerClient = require('../repository/index');
-
+const { handleHTTPRegister, handleHTTPLogin } = require('../controllers/handlers/client');
+const { registerClient, clientLogin } = require('../repository/index');
+ 
 clients.post('/register', (req, res) => {
     const clientData = handleHTTPRegister(req);
     registerClient(clientData, res);
 });
 
 clients.post('/login', (req, res) => {
-    Client.findOne({
-        where: {
-            email: req.body.email
-        }
-    })
-    .then(client => {
-        if(client){
-            if(bcrypt.compareSync(req.body.password, client.password)){
-                let token = jwt.sign(client.dataValues, process.env.SECRET_KEY, {
-                    expiresIn: 1500
-                });
-                res.send(token);
-            }
-        } else {
-            res.status(400).json({error: `Client does not exist`})
-        };
-    })
-    .catch(err => {
-        res.status(400).json({ error: err})
-    });
+    const clientData = handleHTTPLogin(req);
+    clientLogin(clientData, res);
 });
 
 module.exports = clients;
