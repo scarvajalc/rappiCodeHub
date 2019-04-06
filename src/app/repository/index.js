ff57a72d8fbff7006beafe46d7a347079639ecde
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-05 23:42:29 -0500

    Implemented adminLogin method in repository

--- src/app/repository/index.js
+++ src/app/repository/index.js
@@ -1,4 +1,5 @@
 const Client = require('../models/client');
+const Admin = require('../models/admin');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
@@ -50,4 +51,27 @@ const clientLogin = (clientData, res) => {
     });  
 };
 
-module.exports = { registerClient, clientLogin};
+const adminLogin = (adminData, res) => {
+    Admin.findOne({
+        where: {
+            email: adminData.email
+        }
+    })
+    .then(admin => {
+        if(admin){
+            if(bcrypt.compareSync(adminData.password, admin.password)){
+                let token = jwt.sign(admin.dataValues, process.env.SECRET_KEY, {
+                    expiresIn: 1500
+                });
+                res.send(token);
+            }
+        } else {
+            res.status(400).json({error: `Admin does not exist`})
+        };
+    })
+    .catch(err => {
+        res.status(400).json({ error: err})
+    });  
+};
+
+module.exports = { registerClient, clientLogin, adminLogin };
