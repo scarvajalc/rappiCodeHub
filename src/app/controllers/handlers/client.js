82dd86c8d222e66569e3ed6fd37103e310bee5bb
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-05 21:36:07 -0500

    Implemented handleHTTPRegister to read all request body data and transform it to a JSON

---
+++ src/app/controllers/handlers/client.js
@@ -0,0 +1,16 @@
+const handleHTTPRegister = (req) => {
+    const today = new Date();
+    const clientData = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        phone: req.body.phone,
+        email: req.body.email,
+        password: req.body.password,
+        createdAt: today,
+        active: true
+    };
+
+    return clientData;
+};
+
+module.exports = handleHTTPRegister;
