5a87e6e81ec14c4290ec13016a7b6739ed62aa9e
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-05 23:39:33 -0500

    Removed HTTP request admin data handle and findOne method in the /login endpoint - Imported login from handler and login method from repository

--- src/app/controllers/admin.js
+++ src/app/controllers/admin.js
@@ -1,30 +1,12 @@
 const express = require('express');
 const admins = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const Admin = require('../models/admin');
+
+const handleHTTPLogin = require('../controllers/handlers/admin');
+const { adminLogin } = require('../repository/index');
 
 admins.post('/login', (req, res) => {
-    Admin.findOne({
-        where: {
-            email: req.body.email
-        }
-    })
-    .then(admin => {
-        if(admin){
-            if(bcrypt.compareSync(req.body.password, admin.password)){
-                let token = jwt.sign(admin.dataValues, process.env.SECRET_KEY, {
-                    expiresIn: 1500
-                });
-                res.send(token);
-            }
-        } else {
-            res.status(400).json({error: `Admin does not exist`})
-        };
-    })
-    .catch(err => {
-        res.status(400).json({ error: err})
-    });
+    const adminData = handleHTTPLogin(req);
+    adminLogin(adminData, res)
 });
 
 module.exports = admins;
