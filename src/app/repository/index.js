1fdec3f704e8aa5c9306fbaf03839518af26d9ac
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-06 22:10:18 -0500

    Modified repository to export it as a module with adminLogin function

--- src/app/repository/index.js
+++ src/app/repository/index.js
@@ -3,6 +3,30 @@ const Admin = require('../models/admin');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const repository = {
+    adminLogin(adminData, res){
+        Admin.findOne({
+            where: {
+                email: adminData.email
+            }
+        })
+        .then(admin => {
+            if(admin){
+                if(bcrypt.compareSync(adminData.password, admin.password)){
+                    let token = jwt.sign(admin.dataValues, process.env.SECRET_KEY, {
+                        expiresIn: 1500
+                    });
+                    res.send(token);
+                }
+            } else {
+                res.status(400).json({error: `Admin does not exist`})
+            };
+        })
+        .catch(err => {
+            res.status(400).json({ error: err})
+        });  
+    }
+}
 const registerClient = (clientData, res) => {
     Client.findOne({
         where: {
@@ -51,27 +75,4 @@ const clientLogin = (clientData, res) => {
     });  
 };
 
-const adminLogin = (adminData, res) => {
-    Admin.findOne({
-        where: {
-            email: adminData.email
-        }
-    })
-    .then(admin => {
-        if(admin){
-            if(bcrypt.compareSync(adminData.password, admin.password)){
-                let token = jwt.sign(admin.dataValues, process.env.SECRET_KEY, {
-                    expiresIn: 1500
-                });
-                res.send(token);
-            }
-        } else {
-            res.status(400).json({error: `Admin does not exist`})
-        };
-    })
-    .catch(err => {
-        res.status(400).json({ error: err})
-    });  
-};
-
-module.exports = { registerClient, clientLogin, adminLogin };
+module.exports = repository;
