6f15cc7fda2226a47b9bfb034650af5c174297ef
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-06 22:15:06 -0500

    Restructured clientLogin and clientRegister methods to be inside repository module

--- src/app/repository/index.js
+++ src/app/repository/index.js
@@ -4,75 +4,76 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const repository = {
-    adminLogin(adminData, res){
+    adminLogin(adminData, res) {
         Admin.findOne({
             where: {
                 email: adminData.email
             }
         })
-        .then(admin => {
-            if(admin){
-                if(bcrypt.compareSync(adminData.password, admin.password)){
-                    let token = jwt.sign(admin.dataValues, process.env.SECRET_KEY, {
-                        expiresIn: 1500
-                    });
-                    res.send(token);
-                }
-            } else {
-                res.status(400).json({error: `Admin does not exist`})
-            };
+            .then(admin => {
+                if (admin) {
+                    if (bcrypt.compareSync(adminData.password, admin.password)) {
+                        let token = jwt.sign(admin.dataValues, process.env.SECRET_KEY, {
+                            expiresIn: 1500
+                        });
+                        res.send(token);
+                    }
+                } else {
+                    res.status(400).json({ error: `Admin does not exist` })
+                };
+            })
+            .catch(err => {
+                res.status(400).json({ error: err })
+            });
+    },
+
+    registerClient(clientData, res) {
+        Client.findOne({
+            where: {
+                email: clientData.email
+            }
         })
-        .catch(err => {
-            res.status(400).json({ error: err})
-        });  
+            .then(client => {
+                if (!client) {
+                    bcrypt.hash(clientData.password, 10, (err, hash) => {
+                        clientData.password = hash;
+                        Client.create(clientData)
+                            .then(client => {
+                                res.json({ status: `${client.email} registered` })
+                            })
+                            .catch(err => {
+                                res.send(`Error ${err}`)
+                            });
+                    });
+                };
+            })
+            .catch(err => {
+                res.send(`Error ${err}`)
+            });
+    },
+
+    clientLogin(clientData, res) {
+        Client.findOne({
+            where: {
+                email: clientData.email
+            }
+        })
+            .then(client => {
+                if (client) {
+                    if (bcrypt.compareSync(clientData.password, client.password)) {
+                        let token = jwt.sign(client.dataValues, process.env.SECRET_KEY, {
+                            expiresIn: 1500
+                        });
+                        res.send(token);
+                    }
+                } else {
+                    res.status(400).json({ error: `Client does not exist` })
+                };
+            })
+            .catch(err => {
+                res.status(400).json({ error: err })
+            });
     }
 }
-const registerClient = (clientData, res) => {
-    Client.findOne({
-        where: {
-            email: clientData.email
-        }
-    })
-    .then(client => {
-        if(!client){
-            bcrypt.hash(clientData.password, 10, (err, hash) => {
-                clientData.password = hash;
-                Client.create(clientData)
-                .then(client => {
-                    res.json({status: `${client.email} registered`})
-                })
-                .catch(err => {
-                    res.send(`Error ${err}`)
-                });
-            });
-        };
-    })
-    .catch(err => {
-        res.send(`Error ${err}`)
-    });
-};
-
-const clientLogin = (clientData, res) => {
-    Client.findOne({
-        where: {
-            email: clientData.email
-        }
-    })
-    .then(client => {
-        if(client){
-            if(bcrypt.compareSync(clientData.password, client.password)){
-                let token = jwt.sign(client.dataValues, process.env.SECRET_KEY, {
-                    expiresIn: 1500
-                });
-                res.send(token);
-            }
-        } else {
-            res.status(400).json({error: `Client does not exist`})
-        };
-    })
-    .catch(err => {
-        res.status(400).json({ error: err})
-    });  
-};
 
 module.exports = repository;
