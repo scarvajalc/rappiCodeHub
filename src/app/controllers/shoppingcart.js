fad01003872b62b91ce233337e819e3a7b165b7b
Santiago Carvajal <scarvajalc@unal.edu.co>
2019-04-18 17:52:41 -0500

    shopping cart view added

--- src/app/controllers/shoppingcart.js
+++ src/app/controllers/shoppingcart.js
@@ -3,13 +3,13 @@ const sprepository = require('../repository/shoppingcart');
 
 const shoppingCart = {
     async showCart(req, res) {
-        //const clientData = clientHandler.handleHTTPLogin(req);
-        let spresponse = await sprepository.getUserShoppingCart(8);
+        const userId = 8 ////change here for user session
+        let spresponse = await sprepository.getUserShoppingCart(userId); 
         if (spresponse.cartExists && spresponse.cartProducts.length > 0) {
             //req.session.user = repoResponse.clientData;
-            res.render('shoppingCart', {emptyshoppingcart: false, products: spresponse.cartProducts})
+            res.render('shoppingCart', {emptyshoppingcart: false, products: spresponse.cartProducts, total: spresponse.total})
         } else {
-            res.render('shoppingCart', {emptyshoppingcart: true, message: spresponse.message})
+            res.render('shoppingCart', {emptyshoppingcart: true, message: 'No hay items en el carrito'})
         }
     }
 }
--- src/app/repository/shoppingcart.js
+++ src/app/repository/shoppingcart.js
@@ -9,10 +9,14 @@ const sprepository = {
                 client_id: userId
             }
         }).then(cart =>{
-            console.log(cart)
+            //console.log(cart)
             if(cart){
                 return cart.getProducts().then(products =>{
-                        return {cartExists: true, cartProducts: products}
+                    let total = 0
+                    for(let i = 0; i < products.length; i++){
+                        total += products[i].price * products[i].productsshoppingcart.ammount
+                    }
+                    return {cartExists: true, cartProducts: products, total: total}
                 }).catch(err => {
                     return { error: err }
                 });
@@ -24,124 +28,7 @@ const sprepository = {
             return { error: err }
         });
     }
-    /*adminLogin(adminData, res) {
-        Admin.findOne({
-            where: {
-                email: adminData.email
-            }
-        })
-            .then(admin => {
-                if (admin) {
-                    if (bcrypt.compareSync(adminData.password, admin.password)) {
-                        let token = jwt.sign(admin.dataValues, process.env.SECRET_KEY, {
-                            expiresIn: 1500
-                        });
-                        res.send(token);
-                    }
-                } else {
-                    res.status(400).json({ error: `Admin does not exist` })
-                };
-            })
-            .catch(err => {
-                res.status(400).json({ error: err })
-            });
-    },
-
-    clientRegister(clientData, res) {
-        Client.findOne({
-            where: {
-                email: clientData.email
-            }
-        })
-            .then(client => {
-                if (!client) {
-                    bcrypt.hash(clientData.password, 10, (err, hash) => {
-                        clientData.password = hash;
-                        Client.create(clientData)
-                            .then(client => {
-                                res.json({ status: `${client.email} registered` })
-                            })
-                            .catch(err => {
-                                res.send(`Error ${err}`)
-                            });
-                    });
-                };
-            })
-            .catch(err => {
-                res.send(`Error ${err}`)
-            });
-    },
-
-    clientLogin(clientData, res) {
-        Client.findOne({
-            where: {
-                email: clientData.email
-            }
-        })
-            .then(client => {
-                if (client) {
-                    if (bcrypt.compareSync(clientData.password, client.password)) {
-                        let token = jwt.sign(client.dataValues, process.env.SECRET_KEY, {
-                            expiresIn: 1500
-                        });
-                        res.send(token);
-                    }
-                } else {
-                    res.status(400).json({ error: `Client does not exist` })
-                };
-            })
-            .catch(err => {
-                res.status(400).json({ error: err })
-            });
-    },
-
-    rappiTenderoRegister(rappiTenderoData, res) {
-        RappiTendero.findOne({
-            where: {
-                email: rappiTenderoData.email
-            }
-        })
-            .then(rappiTendero => {
-                if (!rappiTendero) {
-                    bcrypt.hash(rappiTenderoData.password, 10, (err, hash) => {
-                        rappiTenderoData.password = hash;
-                        RappiTendero.create(rappiTenderoData)
-                            .then(client => {
-                                res.json({ status: `${rappiTendero.email} registered` })
-                            })
-                            .catch(err => {
-                                res.send(`Error ${err}`)
-                            });
-                    });
-                };
-            })
-            .catch(err => {
-                res.send(`Error ${err}`)
-            });
-    },
-
-    rappiTenderoLogin(rappiTenderoData, res) {
-        RappiTendero.findOne({
-            where: {
-                email: rappiTenderoData.email
-            }
-        })
-            .then(rappiTendero => {
-                if (rappiTendero) {
-                    if (bcrypt.compareSync(rappiTenderoData.password, rappiTendero.password)) {
-                        let token = jwt.sign(rappiTendero.dataValues, process.env.SECRET_KEY, {
-                            expiresIn: 1500
-                        });
-                        res.send(token);
-                    }
-                } else {
-                    res.status(400).json({ error: `RappiTendero does not exist` })
-                };
-            })
-            .catch(err => {
-                res.status(400).json({ error: err })
-            });
-    }*/
+  
 }
 
 module.exports = sprepository;
