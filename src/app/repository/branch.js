63f5e60f11b169d3df602199ea762ef840ffcb19
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-05-05 23:14:22 -0500

    Imported all needed models to get allOpenBranches and getAllBranchProducts methods - Used moment module to get currentTime of server - imported P from sequelize to use less than and greater than operators and implemented both methods mentioned above

--- src/app/repository/branch.js
+++ src/app/repository/branch.js
@@ -1,58 +1,59 @@
+const Branch = require("../models/branch");
+const OpeningHour = require("../models/openinghour");
+const BranchProducts = require("../models/branchproduct");
+const Products = require("../models/product");
+const moment = require("moment");
+const { Op } = require("sequelize");
 
-const Branch = require('../models/branch');
-const OpeningHour = require('../models/openinghour');
-const DateParser = require('../repository/date_parser');
+const branchRepository = {
+  async getOpenBranches() {
+    Branch.hasMany.OpeningHour, { as: "hours", foreignKey: "branch_id" };
+    OpeningHour.belongsTo(Branch, { foreignKey: "id" });
 
-const obrepository = {
-    
-    async getOpenBranches(/*userId*/) {
-    
-        branches = await Branch.findAll({
-            where:{
-                active: true
+    const currentTime = moment().format("HH:mm:ss");
+    const openBranches = await Branch.findAll({
+      where: {
+        active: true
+      },
+      include: [
+        {
+          model: OpeningHour,
+          as: "hours",
+          where: {
+            opening_time: {
+              [Op.lte]: currentTime
+            },
+            closing_time: {
+              [Op.gte]: currentTime
             }
-          })
-        
-        let current_time = DateParser.secsFromWeekStart(new Date())
-        let open_branches = []
-        for(let i = 0; i < branches.length; i++) {
-            let open_branch = false
-            let current_branch = branches[i]
-            let opening_hours = await current_branch.getHours()
-            for(let j = 0; j < opening_hours.length; j++) {
-                if (opening_hours[j].opening_time <= current_time && 
-                    opening_hours[j].closing_time >= current_time) {
-                    open_branch = true
-                    break
-                }
-            }
-            if (open_branch) open_branches.push(current_branch)
+          }
         }
-        return open_branches
-        
-               
-            //date = new Date(); //America/Bogota time zone expected.
-            //seconds since Sun @ 00:00:00
-            //secondsSinceWeekStarted = date.getDay()*86400 + date.getHours()*3600 + date.getMinutes()*60 + date.getSeconds();
-            
-            
-            //console.log(branches)
-            /*if(cart){
-                return cart.getProducts().then(products =>{
-                    let total = 0
-                    for(let i = 0; i < products.length; i++){
-                        total += products[i].price * products[i].productsshoppingcart.ammount
-                    }
-                    return {cartExists: true, cartProducts: products, total: total}
-                }).catch(err => {
-                    return { error: err }
-                });
-            }else{
-                return {cartExists: false, message: 'No hay items en el carrito'}
-            }*/
-            
-    }
+      ]
+    });
 
-}
+    return openBranches;
+  },
 
-module.exports = obrepository;
+  async getAllBranchProducts(branchId) {
+    const branchProducts = await Branch.findAll({
+      where: {
+        id: branchId.id
+      },
+      include: [
+        {
+          model: BranchProducts,
+          required: true,
+          include: [
+            {
+              model: Products,
+              required: true
+            }
+          ]
+        }
+      ]
+    });
+    return branchProducts;
+  }
+};
+
+module.exports = branchRepository;
