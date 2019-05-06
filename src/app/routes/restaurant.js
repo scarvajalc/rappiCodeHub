e932b8847791cde0992a07e9d2c26b486c414e45
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-05-05 23:23:35 -0500

    Created /product/:id route to redirect to the respective restaurant selected and created /singleRestaurant to render the ejs view of the selected restaurant and its respective available products

---
+++ src/app/routes/restaurant.js
@@ -0,0 +1,16 @@
+const branchController = require("../controllers/branch");
+
+module.exports = app => {
+  app.get("/product/:id", (req, res) => {
+    branchController.getAllStoreProducts(req, res);
+  });
+
+  app.get("/singleRestaurant", (req, res) => {
+    const branchName = req.session.products[0].name;
+    const branchProducts = req.session.products[0].branchproducts;
+    res.render("singleRestaurant", {
+      storeProducts: branchProducts,
+      branchName: branchName
+    });
+  });
+};
