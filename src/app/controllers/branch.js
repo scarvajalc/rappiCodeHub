8a8edd1db4aae490759171d5adab6834aca375f6
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-05-08 03:45:18 -0500

    Added checkCartProducts method in branchController [There is some code that should be in another class for sure]

--- src/app/controllers/branch.js
+++ src/app/controllers/branch.js
@@ -1,6 +1,7 @@
 const branchRepository = require("../repository/branch");
 const branchFunctions = require("../functions/branch");
 const branchHandler = require("../controllers/handlers/branch");
+const repository = require("../repository/index");
 
 const branchController = {
   async getAllBranches(req, res) {
@@ -23,6 +24,36 @@ const branchController = {
     );
     req.session.products = branchProducts;
     res.redirect("/singleRestaurant");
+  },
+
+  async checkCartProducts(req, res) {
+    const clientId = req.session.user.id;
+    const cartProducts = await repository.getClientCart(clientId);
+    const cartProductsDataToFilter = branchHandler.handleHTTPCheckCartProducts(
+      cartProducts
+    );
+    const checkedProducts = await branchRepository.checkProducts(
+      cartProductsDataToFilter
+    );
+    /* 100% se que esto debe ir en otro lugar pero son las 2:56 AM
+    y quiero acabar la funcionalidad */
+    var checkedCartProductsResponse = [];
+    for (var i = 0; i < checkedProducts.length; i++) {
+      if (
+        cartProductsDataToFilter[i].quantity >
+          checkedProducts[i][0].dataValues.stock ||
+        !checkedProducts[i][0].dataValues.active
+      ) {
+        checkedCartProductsResponse.push(
+          `${
+            cartProducts[0].cartproducts[i].dataValues.product.dataValues.name
+          } se ha acabado`
+        );
+      }
+    }
+
+    res.send(checkedCartProductsResponse);
+    return checkedCartProductsResponse;
   }
 };
 
